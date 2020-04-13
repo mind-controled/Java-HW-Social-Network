3dc5e72548bf46f523ba183636df0214d1795400
Artem <mind_control@mail.ru>
2020-04-13 19:34:26 +0300

    PostManager Added

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,16 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] search(String[] args) {
+        // logic
+        return posts;
+    }
+
+    public void delete(int postId) {
+        // logic
+    }
+}
