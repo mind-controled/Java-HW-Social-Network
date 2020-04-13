595031a83f402fac444f5bb89da7a7e51eef4976
Artem <mind_control@mail.ru>
2020-04-13 20:41:23 +0300

    Attachments added

---
+++ src/main/java/ru/netology/domain/attachment/PhotosInfo.java
@@ -0,0 +1,14 @@
+package ru.netology.domain.attachment;
+
+public class PhotosInfo {
+    private int id;
+    private int albumId;
+    private int ownerId;
+    private String text;
+    private int date;
+    private int width;
+    private int height;
+    private String photo130;
+    private String photo604;
+
+}
